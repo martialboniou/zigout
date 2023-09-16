9f7d3f6ed8f319c4397344773890d64f4d1176c5
martialboniou <martial@gmx.com>
2023-09-17 01:32:59 +0200

    Upgrade this demo to Zig 0.12

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# About this fork
+
+Use arrow keys to move the brick (A & D were used in the original version)
+
 # Zigout
 
 ![thumbnail](./thumbnail.png)
--- src/main.zig
+++ src/main.zig
@@ -1,32 +1,35 @@
+//
+// code by tsoding + refactoring by hondana (runs on zig 0.12)
+//
 const std = @import("std");
-const math = std.math;
 const c = @cImport({
     @cInclude("SDL2/SDL.h");
 });
 const overlaps = c.SDL_HasIntersection;
 
 const FPS = 60;
-const DELTA_TIME_SEC: f32 = 1.0/@intToFloat(f32, FPS);
+const DELTA_TIME_SEC: f32 = 1.0 / @as(f32, @floatFromInt(FPS));
+const INITIAL_BAR_X: f32 = WINDOW_WIDTH / 2 - BAR_LEN / 2;
 const WINDOW_WIDTH = 800;
 const WINDOW_HEIGHT = 600;
 const BACKGROUND_COLOR = 0xFF181818;
-const PROJ_SIZE: f32 = 25*0.80;
-const PROJ_SPEED: f32 = 350;
+const PROJ_SIZE: f32 = 25;
+const PROJ_SPEED: f32 = 500;
 const PROJ_COLOR = 0xFFFFFFFF;
 const BAR_LEN: f32 = 100;
-const BAR_THICCNESS: f32 = PROJ_SIZE;
+const BAR_THICKNESS: f32 = PROJ_SIZE;
 const BAR_Y: f32 = WINDOW_HEIGHT - PROJ_SIZE - 50;
-const BAR_SPEED: f32 = PROJ_SPEED*1.5;
+const BAR_SPEED: f32 = PROJ_SPEED;
 const BAR_COLOR = 0xFF3030FF;
 const TARGET_WIDTH = BAR_LEN;
-const TARGET_HEIGHT = PROJ_SIZE;
-const TARGET_PADDING_X = 20;
-const TARGET_PADDING_Y = 50;
+const TARGET_HEIGHT = BAR_THICKNESS;
+const TARGET_PADDING_X: f32 = 20;
+const TARGET_PADDING_Y: f32 = 50;
 const TARGET_ROWS = 4;
 const TARGET_COLS = 5;
-const TARGET_GRID_WIDTH = (TARGET_COLS*TARGET_WIDTH + (TARGET_COLS - 1)*TARGET_PADDING_X);
-const TARGET_GRID_X = WINDOW_WIDTH/2 - TARGET_GRID_WIDTH/2;
-const TARGET_GRID_Y = 50;
+const TARGET_GRID_WIDTH = (TARGET_COLS * TARGET_WIDTH + (TARGET_COLS - 1) * TARGET_PADDING_X);
+const TARGET_GRID_X: f32 = WINDOW_WIDTH / 2 - TARGET_GRID_WIDTH / 2;
+const TARGET_GRID_Y: f32 = 50;
 const TARGET_COLOR = 0xFF30FF30;
 
 const Target = struct {
@@ -35,15 +38,15 @@ const Target = struct {
     dead: bool = false,
 };
 
-fn init_targets() [TARGET_ROWS*TARGET_COLS]Target {
-    var targets: [TARGET_ROWS*TARGET_COLS]Target = undefined;
-    var row: usize = 0;
+fn init_targets() [TARGET_ROWS * TARGET_COLS]Target {
+    var targets: [TARGET_ROWS * TARGET_COLS]Target = undefined;
+    var row = 0;
     while (row < TARGET_ROWS) : (row += 1) {
-        var col: usize = 0;
+        var col = 0;
         while (col < TARGET_COLS) : (col += 1) {
-            targets[row*TARGET_COLS + col] = Target {
-                .x = TARGET_GRID_X + (TARGET_WIDTH + TARGET_PADDING_X)*@intToFloat(f32, col),
-                .y = TARGET_GRID_Y + TARGET_PADDING_Y*@intToFloat(f32, row)
+            targets[row * TARGET_COLS + col] = Target{
+                .x = TARGET_GRID_X + (TARGET_WIDTH + TARGET_PADDING_X) * @as(f32, @floatFromInt(col)),
+                .y = TARGET_GRID_Y + TARGET_PADDING_Y * @as(f32, @floatFromInt(row)),
             };
         }
     }
@@ -51,10 +54,10 @@ fn init_targets() [TARGET_ROWS*TARGET_COLS]Target {
 }
 
 var targets_pool = init_targets();
-var bar_x:   f32 = WINDOW_WIDTH/2 - BAR_LEN/2;
-var bar_dx:  f32 = 0;
-var proj_x:  f32 = WINDOW_WIDTH/2 - PROJ_SIZE/2;
-var proj_y:  f32 = BAR_Y - BAR_THICCNESS/2 - PROJ_SIZE;
+var bar_x = WINDOW_WIDTH / 2 - BAR_LEN / 2;
+var bar_dx: f32 = 0;
+var proj_x: f32 = WINDOW_WIDTH / 2 - PROJ_SIZE / 2;
+var proj_y = @as(f32, BAR_Y - BAR_THICKNESS / 2 - PROJ_SIZE); // same as proj_x
 var proj_dx: f32 = 1;
 var proj_dy: f32 = 1;
 var quit = false;
@@ -65,23 +68,23 @@ var started = false;
 // TODO: victory
 
 fn make_rect(x: f32, y: f32, w: f32, h: f32) c.SDL_Rect {
-    return c.SDL_Rect {
-        .x = @floatToInt(i32, x),
-        .y = @floatToInt(i32, y),
-        .w = @floatToInt(i32, w),
-        .h = @floatToInt(i32, h)
+    return c.SDL_Rect{
+        .x = @as(i32, @intFromFloat(x)),
+        .y = @as(i32, @intFromFloat(y)),
+        .w = @as(i32, @intFromFloat(w)),
+        .h = @as(i32, @intFromFloat(h)),
     };
 }
 
 fn set_color(renderer: *c.SDL_Renderer, color: u32) void {
-    const r = @truncate(u8, (color >> (0*8)) & 0xFF);
-    const g = @truncate(u8, (color >> (1*8)) & 0xFF);
-    const b = @truncate(u8, (color >> (2*8)) & 0xFF);
-    const a = @truncate(u8, (color >> (3*8)) & 0xFF);
+    const r: u8 = @truncate((color >> (0 * 8)) & 0xff);
+    const g: u8 = @truncate((color >> (1 * 8)) & 0xff);
+    const b: u8 = @truncate((color >> (2 * 8)) & 0xff);
+    const a: u8 = @truncate((color >> (3 * 8)) & 0xff);
     _ = c.SDL_SetRenderDrawColor(renderer, r, g, b, a);
 }
 
-fn target_rect(target: Target) c.SDL_Rect {
+fn target_rect(target: *const Target) c.SDL_Rect {
     return make_rect(target.x, target.y, TARGET_WIDTH, TARGET_HEIGHT);
 }
 
@@ -90,18 +93,18 @@ fn proj_rect(x: f32, y: f32) c.SDL_Rect {
 }
 
 fn bar_rect(x: f32) c.SDL_Rect {
-    return make_rect(x, BAR_Y - BAR_THICCNESS/2, BAR_LEN, BAR_THICCNESS);
+    return make_rect(x, BAR_Y - BAR_THICKNESS / 2, BAR_LEN, BAR_THICKNESS);
 }
 
 fn horz_collision(dt: f32) void {
-    var proj_nx: f32 = proj_x + proj_dx*PROJ_SPEED*dt;
+    var proj_nx: f32 = proj_x + proj_dx * PROJ_SPEED * dt;
     if (proj_nx < 0 or proj_nx + PROJ_SIZE > WINDOW_WIDTH or overlaps(&proj_rect(proj_nx, proj_y), &bar_rect(bar_x)) != 0) {
         proj_dx *= -1;
         return;
     }
-    for (targets_pool) |*it| {
-        if (!it.dead and overlaps(&proj_rect(proj_nx, proj_y), &target_rect(it.*)) != 0) {
-            it.dead = true;
+    for (targets_pool[0..]) |*target_ptr| { // or &targets_pool, see vert_collision
+        if (!target_ptr.dead and overlaps(&proj_rect(proj_nx, proj_y), &target_rect(target_ptr)) != 0) {
+            target_ptr.dead = true;
             proj_dx *= -1;
             return;
         }
@@ -110,7 +113,7 @@ fn horz_collision(dt: f32) void {
 }
 
 fn vert_collision(dt: f32) void {
-    var proj_ny: f32 = proj_y + proj_dy*PROJ_SPEED*dt;
+    var proj_ny: f32 = proj_y + proj_dy * PROJ_SPEED * dt;
     if (proj_ny < 0 or proj_ny + PROJ_SIZE > WINDOW_HEIGHT) {
         proj_dy *= -1;
         return;
@@ -120,9 +123,9 @@ fn vert_collision(dt: f32) void {
         proj_dy *= -1;
         return;
     }
-    for (targets_pool) |*it| {
-        if (!it.dead and overlaps(&proj_rect(proj_x, proj_ny), &target_rect(it.*)) != 0) {
-            it.dead = true;
+    for (&targets_pool) |*target_ptr| {
+        if (!target_ptr.dead and overlaps(&proj_rect(proj_x, proj_ny), &target_rect(target_ptr)) != 0) {
+            target_ptr.dead = true;
             proj_dy *= -1;
             return;
         }
@@ -131,7 +134,7 @@ fn vert_collision(dt: f32) void {
 }
 
 fn bar_collision(dt: f32) void {
-    var bar_nx : f32 = math.clamp(bar_x + bar_dx*BAR_SPEED*dt, 0, WINDOW_WIDTH - BAR_LEN);
+    var bar_nx: f32 = std.math.clamp(bar_x + bar_dx * BAR_SPEED * dt, 0, WINDOW_WIDTH - BAR_LEN);
     if (overlaps(&proj_rect(proj_x, proj_y), &bar_rect(bar_nx)) != 0) return;
     bar_x = bar_nx;
 }
@@ -139,7 +142,7 @@ fn bar_collision(dt: f32) void {
 fn update(dt: f32) void {
     if (!pause and started) {
         if (overlaps(&proj_rect(proj_x, proj_y), &bar_rect(bar_x)) != 0) {
-            proj_y = BAR_Y - BAR_THICCNESS/2 - PROJ_SIZE - 1.0;
+            proj_y = BAR_Y - BAR_THICKNESS / 2 - PROJ_SIZE - 1.0;
             return;
         }
         bar_collision(dt);
@@ -150,15 +153,16 @@ fn update(dt: f32) void {
 
 fn render(renderer: *c.SDL_Renderer) void {
     set_color(renderer, PROJ_COLOR);
+    _ = c.SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
     _ = c.SDL_RenderFillRect(renderer, &proj_rect(proj_x, proj_y));
 
-    set_color(renderer, BAR_COLOR);
+    _ = c.SDL_SetRenderDrawColor(renderer, 0xff, 0, 0, 0xff);
     _ = c.SDL_RenderFillRect(renderer, &bar_rect(bar_x));
 
-    set_color(renderer, TARGET_COLOR);
     for (targets_pool) |target| {
         if (!target.dead) {
-            _ = c.SDL_RenderFillRect(renderer, &target_rect(target));
+            _ = c.SDL_SetRenderDrawColor(renderer, 0, 0xff, 0, 0xff);
+            _ = c.SDL_RenderFillRect(renderer, &target_rect(&target));
         }
     }
 }
@@ -187,12 +191,14 @@ pub fn main() !void {
     while (!quit) {
         var event: c.SDL_Event = undefined;
         while (c.SDL_PollEvent(&event) != 0) {
-            switch (event.@"type") {
+            switch (event.type) {
                 c.SDL_QUIT => {
                     quit = true;
                 },
                 c.SDL_KEYDOWN => switch (event.key.keysym.sym) {
-                    ' ' => { pause = !pause; },
+                    ' ' => {
+                        pause = !pause;
+                    },
                     else => {},
                 },
                 else => {},
@@ -200,14 +206,13 @@ pub fn main() !void {
         }
 
         bar_dx = 0;
-        if (keyboard[c.SDL_SCANCODE_A] != 0) {
+        if (keyboard[c.SDL_SCANCODE_LEFT] != 0) {
             bar_dx += -1;
             if (!started) {
                 started = true;
                 proj_dx = -1;
             }
-        }
-        if (keyboard[c.SDL_SCANCODE_D] != 0) {
+        } else if (keyboard[c.SDL_SCANCODE_RIGHT] != 0) {
             bar_dx += 1;
             if (!started) {
                 started = true;
@@ -217,13 +222,13 @@ pub fn main() !void {
 
         update(DELTA_TIME_SEC);
 
-        set_color(renderer, BACKGROUND_COLOR);
+        _ = c.SDL_SetRenderDrawColor(renderer, 0x18, 0x18, 0x18, 0xff);
         _ = c.SDL_RenderClear(renderer);
 
         render(renderer);
 
         c.SDL_RenderPresent(renderer);
 
-        c.SDL_Delay(1000/FPS);
+        c.SDL_Delay(1000 / FPS);
     }
 }
